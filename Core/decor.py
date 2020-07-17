437f1e37f12a4917e9e0b364d6f7070aac4e75dc
malorento <68252747+malorento@users.noreply.github.com>
2020-07-17 19:00:05 +0300

    Update decor.py

--- Core/decor.py
+++ Core/decor.py
@@ -1,40 +1,37 @@
 from colorama import Fore
 
-red = Fore.RED
 res = Fore.RESET
 lye = Fore.LIGHTYELLOW_EX
-lge = Fore.LIGHTGREEN_EX
-lbe = Fore.LIGHTBLUE_EX
 lre = Fore.LIGHTRED_EX
 
 
 menu = f"""
-{lge}
-[1] - Find Nickname Information
+{lye}
+[1] - Found Nick Information
 [2] - Find Torrent Information
 [3] - Find Phone or Car Number Information
-{red}[4] - Exit {res}
+{lre}[4] - Exit {res}
 """
 
 chsystem = f"""
-{red}[!]Choose Your System[!]{lge}
+{lre}[!]Choose Your System[!]{lye}
 [1] - Windows
 [2] - Linux/Termux{res}
 """
 
-banner = f""" {lbe}
+banner = f""" {lye}
    _____ _     _
   / ____(_)   | |
  | |  __ _  __| | ___  ___  _ __
  | | |_ | |/ _` |/ _ \/ _ \| '_ \\
  | |__| | | (_| |  __/ (_) | | | |
   \_____|_|\__,_|\___|\___/|_| |_|
-  {lbe}[>>] created and owned by {lre}malorento
-  {lbe}[>>] for channels {lre}"You`ve been HACKED" {lbe}and{lre} "staide"{res}
-  {lbe}[>>] created by the idea of{lre} consulol
+  {lye}[>>] created by {lre}malorento
+  {lye}[>>] for channels {lre}"You`ve been HACKED" {lye}and{lre} "staide"
+  {lye}[>>] the idea belongs to {lre}consulol{res}
 """
 
-sher_menu = f"""{lge}
+sher_menu = f"""{lye}
 [1] - Phone Information
 [2] - Car Information{res}
 """
